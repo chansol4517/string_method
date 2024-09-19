a69f71bc92fa29e14ef9b0724ebb55d5dbbbac24
chansol4517 <ckssol4517@gmail.com>
2024-09-19 15:21:03 +0900

    문자열을 특정 구분자로 배열로 반환, 배열을 특정 구분자로 문자열 이어붙이기

--- script.js
+++ script.js
@@ -1,61 +1,22 @@
-console.log("test");
+//전체 문자열에서 특정 문자값을 기준으로 분리해서 배열로 반환
+const span_text = document.querySelector("span").innerText;
+console.log(span_text);
 
-const h1 = document.querySelector("h1");
-h1.innerText = "test";
+const text_arr = span_text.split("-");
+console.log(text_arr); //['2024', '09', '19'];
 
-const h2 = document.querySelector("h2");
-const linkURL = "https://www.naver.com";
-h2.innerHTML = `<a href=${linkURL}>naver</a>`;
+//배열값은 특정 문자열로 이어서 하나의 문자열로 반환하는 함수
+const new_text = text_arr.join(".");
+console.log(new_text); //2024.09.19
 
-const h3 = document.querySelector("h3");
-// console.log(h3.innerText.length);
-const h3_text = h3.innerText;
+//위의 split과 join을 한번에 같이 map을 활용해서 여산 간소화하는 방법
+// const h2_text = document.querySelector("h2").innerText;
+// console.log(h2_text.split(" "));
 
-//h3 문자열을 0위치에서부터 50번째 위치까지 짜른 뒤
-//짜른 문자열을 다시 h3 innerText 프로퍼티에 대입해서 적용
-// h3.innerText = h3_text.substring(0, 50); //자르고 싶은 위치
+//위의 split과 join을 한번에 연산처리
+const h2_text = document.querySelector("h2").innerText;
+const new_h2 = h2_text.split(" ").join("/");
+console.log(new_h2);
 
-//미션
-//조건문을 활용해서 h3에 들어가는 문자값이 50글자가 안되면 해당 문자를 그대로 출력하고 50글자가 넘어가면 50글자까지 자른 뒤 그 뒤에 ... 말줄임표 추가
-
-/*
-내가 한거
-document.addEventListener("DOMContentLoaded", function () {
-  const h3 = document.querySelector("h3");
-  const h3Text = h3.textContent;
-
-  if (h3Text.length > 50) {
-    h3.textContent = h3Text.slice(0, 50) + "...";
-  }
-});
-*/
-
-//강사님이한거
-/*
-if (h3_text.length > 30) {
-  h3.innerText = h3_text.substring(0, 30) + "...";
-} else {
-  h3.innerText = h3_text;
-}
-*/
-
-/*
-if (h3_text.length > 30) h3.innerText = h3_text.substring(0, 30) + "...";
-else h3.innerText = h3_text;
-*/
-
-h3.innerText = h3_text.length > 30 ? h3_text.substring(0, 30) + "..." : h3_text;
-//미션 - 위의 기능을 재사용하기 편하도록 함수로 패키징
-
-shortenText("h1", 5);
-shortenText("h2", 5);
-shortenText("h3", 20);
-function shortenText(elem, len) {
-  //첫 번째 인수로 받은 문자값으로 DOM요소 선택
-  const el = document.querySelector(elem);
-  //해당 요소의 문자값 변수에 할당
-  const el_text = el.innerText;
-  //해당 돔요소에 innerText 프로퍼티에 대입되는 우항을 3항연산자로 연산 처리후 대입
-  el.innerText =
-    el_text.length > len ? el_text.substring(0, len) + "..." : el_text;
-}
+//미션 - 위의 구문을 다음과 같은 형태의 함수로 패키징
+//changeText('h2',' ','/') 3시 25분까지 완성
